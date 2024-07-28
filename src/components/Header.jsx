a5d1910949de72ee27fdd85e10bb49f000ebf022
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-07-28 23:03:11 +0530

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <div>logo</div>
         </div>
         <div>
-            <div className='flex gap-2'>
+            <div className='flex gap-2 justify-center items-center'>
                 <div className='h-3/4 w-[70px] bg-blue-400 rounded-xl flex justify-center'>$0.00</div>
                 <TonConnectButton />
                 
