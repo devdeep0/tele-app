2ed19c10b1fd36e17f57fee4d8118b469ba3971d
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-07-28 23:01:38 +0530

    changed

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { TonConnectButton } from '@tonconnect/ui-react'
+function Header() {
+  return (
+    <div className='h-20 flex justify-between m-2 items-center'>
+        <div>
+        <div>logo</div>
+        </div>
+        <div>
+            <div className='flex gap-2'>
+                <div className='h-3/4 w-[70px] bg-blue-400 rounded-xl flex justify-center'>$0.00</div>
+                <TonConnectButton />
+                
+            </div>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Header
