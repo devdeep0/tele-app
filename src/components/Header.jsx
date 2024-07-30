f58a21ecbee5f4d355f1db58f3d84f035fa104a0
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-07-30 22:07:19 +0530

    theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { TonConnectButton } from '@tonconnect/ui-react'
 function Header() {
   return (
-    <div className='h-20 flex justify-between m-2 items-center'>
+    <div className='h-20 fixed top-0 p-4 flex justify-between m-2 w-full items-center'>
         <div>
         <div>logo</div>
         </div>
         <div>
-            <div className='flex gap-2 justify-center items-center'>
+            <div className='flex gap-2 fixded justify-center items-center bg-transparent'>
                 <div className='h-3/4 w-[70px] bg-blue-400 rounded-xl  flex justify-center'>$0.00</div>
                 <TonConnectButton />
                 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,10 +7,13 @@ function Main() {
     const rawAddress = useTonAddress(true);
     const shortAddress = userFriendlyAddress ? userFriendlyAddress.slice(0, 4)+ '...': '';
   return (
-    <main className='p-4' >
+    <main className='p-4 pt-[80px]' style={{
+      background: 'radial-gradient(circle, rgba(43,82,183,1) 0%, rgba(104,73,192,1) 32%, rgba(43,82,183,1) 70%, rgba(4,4,40,1) 100%)',
+      height: '100vh', // Ensure the background covers the entire viewport
+    }}>
         <div className='flex gap-4 flex-col'>
         <div className='text-blue-400 text-3xl font-bold'>Hey {shortAddress}, <br /> Ready to play?</div>
-        <div className='text-gray-500'>Categories</div>
+        <div className='text-white'>Categories</div>
         <div className='flex gap-5 max-w-full overflow-x-auto'>
             <div className='bg-stone-900 p-3 rounded-3xl'>shooting</div>
             <div className='bg-stone-900 p-3 rounded-3xl'>arena</div>
@@ -18,7 +21,7 @@ function Main() {
             <div className='bg-stone-900 p-3 rounded-3xl'>missionary</div>
             <div className='bg-stone-900 p-3 rounded-3xl'>69</div>
         </div>
-        <div className='text-gray-500'>Most Played</div>
+        <div className='text-white'>Most Played</div>
         <div className='flex overflow-x-auto gap-4 max-w-full'>
           <div className='h-48 w-[150px] bg-blue-400 rounded-xl'></div>
         </div>
